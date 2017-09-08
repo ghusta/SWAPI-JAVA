2b8edb428dabb85e13c599240ad065dba6bd7e56
Guillaume HUSTA <guillaume.husta@gmail.com>
2017-09-08 10:39:59 +0200

    Add log for http requests (okhttp)

--- src/main/java/com/swapi/sw/StarWarsApi.java
+++ src/main/java/com/swapi/sw/StarWarsApi.java
@@ -3,9 +3,17 @@ package com.swapi.sw;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.swapi.APIConstants;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Oleur on 22/12/2014.
@@ -13,11 +21,16 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  */
 public class StarWarsApi {
 
+    private static final Logger log = LoggerFactory.getLogger(StarWarsApi.class);
+
     private StarWars swService;
     private static StarWarsApi instance;
 
     private StarWarsApi() {
-        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(new RequestLoggingInterceptor());
 
         // TODO: add header User-Agent
 
@@ -35,6 +48,20 @@ public class StarWarsApi {
             instance = new StarWarsApi();
         }
         return instance.swService;
+    }
+
+    private static class RequestLoggingInterceptor implements Interceptor
+    {
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            log.debug("HTTP Request : {}", request.toString());
+            log.debug("HTTP Request headers : {}", request.headers().toString());
+
+            Response response = chain.proceed(request);
+            return response;
+        }
     }
 
 }
