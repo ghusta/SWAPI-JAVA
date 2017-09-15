34c6ceea79a7739162192d268390549a0a8905d5
Guillaume HUSTA <guillaume.husta@gmail.com>
2017-09-15 12:27:32 +0200

    Fix Sonar rules : Utility classes should not have public constructors

--- src/main/java/com/swapi/APIConstants.java
+++ src/main/java/com/swapi/APIConstants.java
@@ -5,6 +5,11 @@ package com.swapi;
  * API constants
  */
 public class APIConstants {
+
+    private APIConstants() {
+    }
+
     public static final String BASE_URL = "https://swapi.co/api/";
     public static final String USER_AGENT_NAME = "SWAPI-Java-Client/1.0";
+
 }
