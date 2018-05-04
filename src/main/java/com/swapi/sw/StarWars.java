304df871cf101847ec82b35994af0c2ce1b7b6a9
Guillaume Husta <g.husta@free.fr>
2018-05-04 23:42:09 +0200

    Clean up

--- src/main/java/com/swapi/sw/StarWars.java
+++ src/main/java/com/swapi/sw/StarWars.java
@@ -20,42 +20,42 @@ import retrofit2.http.Query;
 public interface StarWars {
 
     @GET("")
-    public Call<Root> getRootUrls();
+    Call<Root> getRootUrls();
 
     @GET("people/")
-    public Call<SWModelList<People>> getAllPeople(@Query("page") int page);
+    Call<SWModelList<People>> getAllPeople(@Query("page") int page);
 
     @GET("people/{id}/")
-    public Call<People> getPeople(@Path("id") int peopleId);
+    Call<People> getPeople(@Path("id") int peopleId);
 
     @GET("films/")
-    public Call<SWModelList<Film>> getAllFilms(@Query("page") int page);
+    Call<SWModelList<Film>> getAllFilms(@Query("page") int page);
 
     @GET("films/{id}/")
-    public Call<Film> getFilm(@Path("id") int filmId);
+    Call<Film> getFilm(@Path("id") int filmId);
 
     @GET("starships")
-    public Call<SWModelList<Starship>> getAllStarships(@Query("page") int page);
+    Call<SWModelList<Starship>> getAllStarships(@Query("page") int page);
 
     @GET("starships/{id}/")
-    public Call<Starship> getStarship(@Path("id") int starshipId);
+    Call<Starship> getStarship(@Path("id") int starshipId);
 
     @GET("vehicles/")
-    public Call<SWModelList<Vehicle>> getAllVehicles(@Query("page") Integer page);
+    Call<SWModelList<Vehicle>> getAllVehicles(@Query("page") Integer page);
 
     @GET("vehicles/{id}/")
-    public Call<Vehicle> getVehicle(@Path("id") int vehicleId);
+    Call<Vehicle> getVehicle(@Path("id") int vehicleId);
 
     @GET("species/")
-    public Call<SWModelList<Species>> getAllSpecies(@Query("page") int page);
+    Call<SWModelList<Species>> getAllSpecies(@Query("page") int page);
 
     @GET("species/{id}/")
-    public Call<Species> getSpecies(@Path("id") int speciesId);
+    Call<Species> getSpecies(@Path("id") int speciesId);
 
     @GET("planets/")
-    public Call<SWModelList<Planet>> getAllPlanets(@Query("page") Integer page);
+    Call<SWModelList<Planet>> getAllPlanets(@Query("page") Integer page);
 
     @GET("planets/{id}/")
-    public Call<Planet> getPlanet(@Path("id") int planetId);
+    Call<Planet> getPlanet(@Path("id") int planetId);
 
 }
